0a373c32ba7f8495d8b27f51056ea4d377ad7e8d
Benoit Brayer <benoit.brayer@alma.fr>
2016-11-15 17:06:17 +0100

    Trying to solve urls pattern bug since django urlpatterns syntax changed.

--- django_evercookie/urls.py
+++ django_evercookie/urls.py
@@ -1,14 +1,15 @@
 try:
-    from django.conf.urls import url, patterns
+    from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import url, patterns
+    from django.conf.urls.defaults import url
 
 """URLs differ from standart evercookie_<storage_method> to dodge easyprivacy blocking rules"""
 
 
-urlpatterns = patterns('django_evercookie.views',
+urlpatterns = [
     url(r'^ecache', 'evercookie_cache', name='ecache'),
     url(r'^epng', 'evercookie_png', name='epng'),
     url(r'^ecetag', 'evercookie_etag', name='ecetag'),
     url(r'^ecookie', 'evercookie_core', name='ecookie'),
     url(r'^ecauth', 'evercookie_auth', name='ecauth'), )
+]
