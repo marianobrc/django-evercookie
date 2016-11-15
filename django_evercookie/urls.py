9c872c619f59bfbdd4c86ee0e249e07af3efd0e3
Benoit Brayer <benoit.brayer@alma.fr>
2016-11-15 17:16:10 +0100

    Still trying to fix urls.

--- django_evercookie/urls.py
+++ django_evercookie/urls.py
@@ -1,15 +1,20 @@
-try:
-    from django.conf.urls import url
-except ImportError:
-    from django.conf.urls.defaults import url
+"""URLs differ from standart evercookie_<storage_method> to dodge easyprivacy blocking rules."""
 
-"""URLs differ from standart evercookie_<storage_method> to dodge easyprivacy blocking rules"""
+from django.conf.urls import \
+    url
+from views import (
+    evercookie_cache,
+    evercookie_png,
+    evercookie_etag,
+    evercookie_core,
+    evercookie_auth
+)
 
 
 urlpatterns = [
-    url(r'^ecache', 'evercookie_cache', name='ecache'),
-    url(r'^epng', 'evercookie_png', name='epng'),
-    url(r'^ecetag', 'evercookie_etag', name='ecetag'),
-    url(r'^ecookie', 'evercookie_core', name='ecookie'),
-    url(r'^ecauth', 'evercookie_auth', name='ecauth'),
+    url(r'^ecache', evercookie_cache, name='ecache'),
+    url(r'^epng', evercookie_png, name='epng'),
+    url(r'^ecetag', evercookie_etag, name='ecetag'),
+    url(r'^ecookie', evercookie_core, name='ecookie'),
+    url(r'^ecauth', evercookie_auth, name='ecauth'),
 ]
